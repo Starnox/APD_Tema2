609f5ed4c5563d0394d45ea86ee7bb7ff6184915
Mihailescu Eduard-Florin <eduard.mihailescu1@yahoo.ca>
2022-12-08 14:24:46 +0200

    Initial commit

---
+++ src/com/test/Tema2.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Tema2 {
+    // TODO Extract the folder name from the argument
+    // TODO Extract the maximum number of threads from the argument and store it in a variable P
+    // TODO Read the two files from the folder
+    // TODO Create a parallel approach using ExecutorService with maximum P threads
+    // TODO Create Makefile
+    
+    // TODO Add version control
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
